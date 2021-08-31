447d0a7b79dabfa43ea9fb4eae8c4201ed847dc2
yashaskm11 <76162061+yashaskm11@users.noreply.github.com>
2021-08-31 15:34:04 +0530

    main.sh
    Shell Script to Create Hardlinks

---
+++ main.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# $0= No of Files to create Hardlinks
+# $1= Path to existing directory
+# $2= Path to create Hardlinks
+echo "$1 $2 $3"
+ls "$2" > list.txt
+j=`expr $1 + 1`
+i=1
+while [ $i -lt $j ]
+do
+  echo $j
+  file="`head -$i "list.txt" | tail -1`"
+  echo "$file"
+  ln "$2$file" "$3$file"
+  i=`expr $i + 1`
+done
+rm list.txt
